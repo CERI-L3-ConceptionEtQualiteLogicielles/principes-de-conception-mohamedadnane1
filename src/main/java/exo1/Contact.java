2b4dbff03fa2782d86d0c96c0d7215d65f549ae9
mohamedadnane1 <mohamedadnanelycee@gmail.com>
2025-03-13 11:12:13 +0100

    changement sur contact pour refactor pour respecter le principe SOLID

--- src/main/java/exo1/Contact.java
+++ src/main/java/exo1/Contact.java
@@ -1,17 +1,12 @@
 package exo1;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Contact implements ContactService {
-    private static List<Contact> lesContacts = new ArrayList<>();
+public class Contact {
     private String nom;
     private String numero;
 
     public Contact(String nom, String numero) {
         this.nom = nom;
         this.numero = numero;
-        lesContacts.add(this);
     }
 
     public String getNom() {
@@ -22,35 +17,8 @@ public class Contact implements ContactService {
         return numero;
     }
 
-    public String getInfoContact() {
-        return "Nom: " + getNom() + ", Numéro: " + getNumero();
-    }
-
-    public void ajouteContact(Contact contact) {
-        lesContacts.add(contact);
-    }
-
-    public void supprimeContact(Contact contact) {
-        lesContacts.remove(contact);
-    }
-
-    public void afficheContacts() {
-        for (Contact contact : lesContacts) {
-            System.out.println("Nom: " + contact.getNom() + ", Numéro: " + contact.getNumero());
-        }
-    }
-
-    public void sauvegardeEnBD() {
-        // Logique pour sauvegarder les contacts dans une base de données
-    }
-
-    public void envoiEmail(Contact contact, String message) {
-        // Logique pour envoyer un email
-    }
-
     @Override
     public String toString() {
-        return "Nom: " + getNom() + ", Numéro: " + getNumero();
+        return "Nom: " + nom + ", Numéro: " + numero;
     }
 }
-
