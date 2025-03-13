bb4972b3b1ac7cb76d6d8bd679a243f57a41d7eb
mohamedadnane1 <mohamedadnanelycee@gmail.com>
2025-03-13 16:55:53 +0100

    Ajout de la gestion des formats d'affichage pour Contact : - Implémentation de la méthode getInfoContact() avec un switch case - Prise en charge des formats TEXT, XML et JSON - Ajout de la méthode setFormat() pour changer dynamiquement le format - Mise à jour de Main pour tester les différents formats

--- src/main/java/exo1/Contact.java
+++ src/main/java/exo1/Contact.java
@@ -3,10 +3,12 @@ package exo1;
 public class Contact {
     private String nom;
     private String numero;
+    private String format; // Permet de stocker le format actuel (TEXT, XML, JSON)
 
     public Contact(String nom, String numero) {
         this.nom = nom;
         this.numero = numero;
+        this.format = "TEXT"; // Format par défaut
     }
 
     public String getNom() {
@@ -17,9 +19,24 @@ public class Contact {
         return numero;
     }
 
-    @Override
-    public String toString() {
-        return "Nom: " + nom + ", Numéro: " + numero;
+    public void setFormat(String format) {
+        this.format = format.toUpperCase();
     }
 
+    public String getInfoContact() {
+        switch (format) {
+            case "XML":
+                return "<contact>\n\t<nom>" + nom + "</nom>\n\t<numero>" + numero + "</numero>\n</contact>";
+            case "JSON":
+                return "{\n\t\"nom\": \"" + nom + "\",\n\t\"numero\": \"" + numero + "\"\n}";
+            case "TEXT":
+            default:
+                return "Nom: " + nom + ", Numéro: " + numero;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getInfoContact();
+    }
 }
--- src/main/java/exo1/Main.java
+++ src/main/java/exo1/Main.java
@@ -1,30 +1,20 @@
 package exo1;
 
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-
 public class Main {
     public static void main(String[] args) {
-
-        ServiceEnvoiPhoto servicePhoto = new ServiceEnvoiPhoto();
-
-
-        EmailService emailService = new EmailService() {
-            @Override
-            public void envoiEmail(Contact contact, String message) {
-                System.out.println("Email envoyé à " + contact.getNom() + " : " + message);
-            }
-        };
-
-
-        ContactManager manager = new ContactManager(emailService, servicePhoto);
-
-
         Contact c1 = new Contact("Alice", "123456789");
-        manager.ajouteContact(c1);
 
+        System.out.println("Test des formats d'affffichage :");
 
-        Image photo = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
-        manager.envoyerPhoto(c1, photo);
+        System.out.println("\nFormat Texte :");
+        System.out.println(c1.getInfoContact());
+
+        c1.setFormat("XML");
+        System.out.println("\nFormat XML :");
+        System.out.println(c1.getInfoContact());
+
+        c1.setFormat("JSON");
+        System.out.println("\nFormat JSON :");
+        System.out.println(c1.getInfoContact());
     }
 }
