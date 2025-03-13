c0ef549339c51dc42ba2e0f6ea8902b23e29dc48
mohamedadnane1 <mohamedadnanelycee@gmail.com>
2025-03-13 17:45:05 +0100

    Ajout de la classe Contact avec immuabilité

--- src/main/java/exo1/Contact.java
+++ src/main/java/exo1/Contact.java
@@ -1,42 +1,62 @@
 package exo1;
 
-public class Contact {
-    private String nom;
-    private String numero;
-    private String format; // Permet de stocker le format actuel (TEXT, XML, JSON)
+import java.time.LocalDate;
 
-    public Contact(String nom, String numero) {
-        this.nom = nom;
-        this.numero = numero;
-        this.format = "TEXT"; // Format par défaut
+public final class Contact {
+    private final String nom;
+    private final String numero;
+    private final String prenom;
+    private final String civilite;
+    private final String adressePostale;
+    private final String adresseMail;
+    private final LocalDate dateAnniversaire;
+    private final String lieuTravail;
+    private final String ville;
+
+
+    private Contact(ContactBuilder builder) {
+        this.nom = builder.nom;
+        this.numero = builder.numero;
+        this.prenom = builder.prenom;
+        this.civilite = builder.civilite;
+        this.adressePostale = builder.adressePostale;
+        this.adresseMail = builder.adresseMail;
+        this.dateAnniversaire = builder.dateAnniversaire;
+        this.lieuTravail = builder.lieuTravail;
+        this.ville = builder.ville;
     }
 
     public String getNom() {
-        return nom;
-    }
-
+        return nom; }
     public String getNumero() {
-        return numero;
-    }
-
-    public void setFormat(String format) {
-        this.format = format.toUpperCase();
-    }
-
-    public String getInfoContact() {
-        switch (format) {
-            case "XML":
-                return "<contact>\n\t<nom>" + nom + "</nom>\n\t<numero>" + numero + "</numero>\n</contact>";
-            case "JSON":
-                return "{\n\t\"nom\": \"" + nom + "\",\n\t\"numero\": \"" + numero + "\"\n}";
-            case "TEXT":
-            default:
-                return "Nom: " + nom + ", Numéro: " + numero;
-        }
-    }
+        return numero; }
+    public String getPrenom() {
+        return prenom; }
+    public String getCivilite() {
+        return civilite; }
+    public String getAdressePostale() {
+        return adressePostale; }
+    public String getAdresseMail() {
+        return adresseMail; }
+    public LocalDate getDateAnniversaire() {
+        return dateAnniversaire; }
+    public String getLieuTravail() {
+        return lieuTravail; }
+    public String getVille() {
+        return ville; }
 
     @Override
     public String toString() {
-        return getInfoContact();
+        return "Contact{" +
+                "nom='" + nom + '\'' +
+                ", numero='" + numero + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", civilite='" + civilite + '\'' +
+                ", adressePostale='" + adressePostale + '\'' +
+                ", adresseMail='" + adresseMail + '\'' +
+                ", dateAnniversaire=" + dateAnniversaire +
+                ", lieuTravail='" + lieuTravail + '\'' +
+                ", ville='" + ville + '\'' +
+                '}';
     }
 }
